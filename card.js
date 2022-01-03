ce3102aad9bd1b7f9239b92b020b4b35249f92ee
Jayson Tian <48577877+Jaysontian@users.noreply.github.com>
2022-01-03 17:16:52 -0500

    add

--- card.js
+++ card.js
@@ -15,11 +15,13 @@ var data = {
                     {
                         id: 152423,
                         type:'text',
+                        level: 1,
                         content:'<b>Mocha</b> is many things in one – a note-taking app, a study and learning tool, an outliner, a document editor, and much more!'
                     },
                     {
                         id: 1523556,
                         type:'text',
+                        level: 1,
                         content:"It is packed with features and is suitable for anyone, whether you're a student, teacher, or a creative."
                     }
                 ]
@@ -32,21 +34,25 @@ var data = {
                     {
                         id: 4124343,
                         type:'text',
+                        level: 1,
                         content:'Cards are singular blocks that house checks, topics, and notes. This block of white text is a card.'
                     },
                     {
                         id: 124353524124,
                         type:'text',
+                        level: 1,
                         content:'Add a new card by clicking the + New Card button at the bottom of the page or use the keyboard shortcut ⌘ + N'
                     },
                     {
                         id: 1242423248,
                         type:'text',
+                        level: 1,
                         content:'Rearrange the order of cards on a page by hovering over the three dots on the right most corner of the card and dragging it to the desired location.'
                     },
                     {
                         id: 9747937892,
                         type:'text',
+                        level: 1,
                         content:'Delete a card by clicking the X button below the three dots'
                     }
                 ]
@@ -59,11 +65,13 @@ var data = {
                     {
                         id: 23328,
                         type:'text',
+                        level: 1,
                         content:'Mocha is most powerful when connected with Google: you are able to send your pages directly to your google drive.'
                     },
                     {
                         id: 82765673,
                         type:'text',
+                        level: 2,
                         content:'By clicking the Send to Drive button at the top right of the page, a PDF of your page will be generated in your google drive instantly'
                     },
                     {
@@ -74,6 +82,7 @@ var data = {
                     {
                         id: 7636327887,
                         type:'text',
+                        level: 1,
                         content:'Click on the PDF button to be redirected to the PDF in your drive. You can then share your page with friends, download, or print it.'
                     }
                 ]
@@ -104,7 +113,8 @@ function renderPage(pageId){
         axis:'y',
         opacity: 0.5,
         scroll: true,
-        scrollSensitivity: 50,
+        scrollSensitivity: 300,
+        scrollSpeed: 30,
         helper: "clone",
         placeholder: 'ui-state-highlight',
         over: function(event, ui) {
@@ -191,6 +201,12 @@ var render = {
                 } else {
                     $('#'+prevId).after(notecontainer);
                 }
+            };
+            var noteIndex = data[pageId].cards[cardIndex].notes.findIndex(function(note) {
+                return note.id == noteId
+            });
+            if (data[pageId].cards[cardIndex].notes[noteIndex].level == 2){
+                notecontainer.addClass('level-2');
             }
             noteDIV = $('#'+noteId).find('.editor').focus();
             var noteIndex = data[pageId].cards[cardIndex].notes.findIndex(function(note) {
@@ -216,8 +232,23 @@ var render = {
                         create.note.text(pageId, cardIndex, noteId);
                     }
                 }
-                if (e.keyCode == 8 && noteDIV.text()==''){
-                    remove.note.text(pageId, cardIndex, noteId)
+                console.log(e.key);
+                if (e.key == "Shift" && e.key == "Tab"){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    console.log('ah')
+                }
+
+                if (e.keyCode == 9){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    editorEvents.tab(pageId, cardIndex, noteId);
+                }
+
+                if (noteDIV.text()==''){
+                    if (e.keyCode == 8){
+                        remove.note.text(pageId, cardIndex, noteId)
+                    }    
                 }
             });
             noteDIV.on("paste", function(e){
@@ -266,6 +297,7 @@ var render = {
             } else {
                 $('#'+replaceId).replaceWith(image);
             }
+            // selection, deletion, insertion
             image.find('.image').attr('id','img-'+noteId).attr('src', data[pageId].cards[cardIndex].notes[noteIndex].link).click((e)=>{
                 image.toggleClass('focus');
                 console.log('toggled '+noteId)
@@ -275,7 +307,6 @@ var render = {
                     image.removeClass('focus');
                 }
             });
-
             $(window).keydown((e)=>{
                 if (e.keyCode == 13){
                     e.preventDefault();
@@ -288,7 +319,22 @@ var render = {
                         remove.note.image(pageId, cardIndex, $('.focus').attr('id'));
                     }
                 }
+                if (e.keyCode == 16){
+                    image.find('.image-wrapper').resizable({aspectRatio: true})
+                }
             });
+            $(window).keyup((e)=>{
+                image.find('.image-wrapper').resizable({aspectRatio: false})
+            })
+
+            image.find('.image-wrapper').resizable({
+                handles: "se",
+                minWidth: 200,
+                maxWidth: 450,
+                minHeight: 200,
+                containment: "document",
+                maxHeight:'none',
+            })
             
         },
     }
@@ -318,7 +364,7 @@ var modify = {
 
 var create = {
     note: {
-        text: (pageId, cardIndex, prevId, before) => {
+        text: (pageId, cardIndex, prevId, before, level) => {
             var newID = Math.random().toString(16).slice(2);
             var index = data[pageId].cards[cardIndex].notes.findIndex(function(note) {
                 return note.id == prevId
@@ -326,10 +372,11 @@ var create = {
             (data[pageId].cards[cardIndex].notes).splice(parseInt(index)+1, 0, {
                 id: newID,
                 type: 'text',
-                content: ''
+                content: '',
+                level: 1,
             })
             update();
-            render.note.text(pageId, cardIndex, newID, prevId, before);
+            render.note.text(pageId, cardIndex, newID, prevId, before, level);
             //console.log(data[pageId].cards[cardId].notes, data[pageId].cards[cardId].topic);
         },
         image: (pageId, cardIndex, replaceId, imglink) => {
@@ -404,7 +451,20 @@ var remove = {
             }
         }
     }
+}
 
+var editorEvents = {
+    tab: (pageId, cardIndex, noteId)=>{
+        var noteIndex = data[pageId].cards[cardIndex].notes.findIndex(function(note) {
+            return note.id == noteId
+        });
+        if (data[pageId].cards[cardIndex].notes[noteIndex].level != 2){
+            $('#'+noteId).addClass('level-2');
+
+            data[pageId].cards[cardIndex].notes[noteIndex].level = 2;
+            update();    
+        }
+    }
 }
 
 function print(pageId, cardIndex){
